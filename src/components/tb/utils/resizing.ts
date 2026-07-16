129297224bb8507571036d082e34e927ed452b8b
Marwan Hilal <marwan.hilal.dev+1@gmail.com>
2026-07-16 13:57:21 +0200

    fix some resizing and layout issues

--- src/components/tb/utils/resizing.ts
+++ src/components/tb/utils/resizing.ts
@@ -5,21 +5,24 @@ export function generateOnResizerPointerDown(col: Col) {
         this.classList.add("wl--active");
 
         const originX = ptrDownEvent.clientX;
-        const resizerHalfWidth = this.offsetWidth / 2;
         let resizeBy = 0;
         const onPointerMove = (e: PointerEvent) => {
             e.preventDefault();
-            if (col.canNotResizeBy(e.clientX - resizerHalfWidth - originX)) return;
-            resizeBy = e.clientX - resizerHalfWidth - originX;
-            this.style.insetInlineStart = `${e.clientX - resizerHalfWidth}px`;
-            this.style.insetInlineEnd = `unset`;
+            if (col.canNotResizeBy(e.clientX - originX)) {
+                document.documentElement.style.cursor = "not-allowed";
+                return;
+            }
+            document.documentElement.style.removeProperty("cursor");
+            resizeBy = e.clientX - originX;
+            this.style.insetInlineEnd = `calc(anchor(--th-anchor end) - 5px - ${resizeBy}px)`;
         };
         const onPointerUp = () => {
             this.classList.remove("wl--active");
             if (resizeBy !== 0) col.resizeBy(resizeBy);
+            console.log(resizeBy);
             document.removeEventListener("pointermove", onPointerMove);
             this.style.removeProperty("inset-inline-end");
-            this.style.removeProperty("inset-inline-start");
+            document.documentElement.style.removeProperty("cursor");
         };
 
         document.addEventListener("pointerup", onPointerUp, { once: true });
