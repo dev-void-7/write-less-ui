a3df78f5bce3a0fd9bb6a0735bbab41350304b9f
Marwan Hilal <marwan.hilal.dev+1@gmail.com>
2026-07-25 13:19:49 +0200

    oraganize and comment codes

--- src/components/tb/utils/watch-scrollbar-visibility.ts
+++ src/components/tb/utils/watch-scrollbar-visibility.ts
@@ -6,29 +6,40 @@ export function watchScrollbarVisibility(
     thumb: HTMLButtonElement,
 ) {
     let scrollHandler: ((e: Event) => void) | undefined;
-    let wheelHandler: ((e: WheelEvent) => void) | undefined;
+    let thumbWrapperWheelHandler: ((e: WheelEvent) => void) | undefined;
     const observer = new ResizeObserver((_) => {
         if (isTrulyScrollable(tbWrapper)) {
             verticalScrollbar.classList.add("wl--show");
+
             const wrapperHeight = parseFloat(getComputedStyle(tbWrapper).height);
             const contentHeight = parseFloat(getComputedStyle(tb).height);
             const thumbWrapperHeight = parseFloat(getComputedStyle(thumbWrapper).height);
             const thumbHeight = thumbWrapperHeight * (wrapperHeight / contentHeight);
+            const availableWrapperScrolling = contentHeight - wrapperHeight;
+
             thumb.style.height = `${thumbHeight}px`;
+
             if (scrollHandler) tbWrapper.removeEventListener("scroll", scrollHandler);
-            if (wheelHandler) tbWrapper.removeEventListener("wheel", wheelHandler);
+            if (thumbWrapperWheelHandler)
+                tbWrapper.removeEventListener("wheel", thumbWrapperWheelHandler);
+
             scrollHandler = generateScrollHandler(
-                contentHeight - wrapperHeight,
+                availableWrapperScrolling,
                 thumbWrapperHeight - thumbHeight,
                 thumb,
             );
-            wheelHandler = generateWheelHandler(tbWrapper);
+            thumbWrapperWheelHandler = generateThumbWrapperWheelHandler(tbWrapper);
+
             tbWrapper.addEventListener("scroll", scrollHandler);
-            verticalScrollbar.addEventListener("wheel", wheelHandler, { passive: false });
+            verticalScrollbar.addEventListener("wheel", thumbWrapperWheelHandler, {
+                passive: false,
+            });
         } else {
             verticalScrollbar.classList.remove("wl--show");
+
             if (scrollHandler) tbWrapper.removeEventListener("scroll", scrollHandler);
-            if (wheelHandler) verticalScrollbar.removeEventListener("wheel", wheelHandler);
+            if (thumbWrapperWheelHandler)
+                verticalScrollbar.removeEventListener("wheel", thumbWrapperWheelHandler);
         }
     });
 
@@ -58,9 +69,43 @@ function generateScrollHandler(
     };
 }
 
-function generateWheelHandler(contentWrapper: HTMLDivElement) {
+function generateThumbWrapperWheelHandler(contentWrapper: HTMLDivElement) {
     return function (e: WheelEvent) {
         e.preventDefault();
         contentWrapper.scrollTop += e.deltaY;
     };
 }
+
+// function generateTbWrapperWheelHandler(availableWrapperScrolling: number) {
+//     return function (this: HTMLElement, e: WheelEvent) {
+//         if (e.deltaY == 0) return;
+//         if (e.deltaY > 0) {
+//             const wrapperRemainingScroll = availableWrapperScrolling - this.scrollTop;
+//             if (wrapperRemainingScroll >= e.deltaY) return;
+
+//             e.preventDefault();
+//             e.stopImmediatePropagation();
+//             e.stopPropagation();
+//             this.scrollBy({ top: wrapperRemainingScroll, behavior: "auto" });
+//             if (this.parentElement) {
+//                 this.parentElement.scrollBy({
+//                     top: e.deltaY - wrapperRemainingScroll,
+//                     behavior: "auto",
+//                 });
+//             }
+//         } else if (e.deltaY < 0) {
+//             if (this.scrollTop + e.deltaY >= 0) return;
+//             e.preventDefault();
+//             e.stopImmediatePropagation();
+//             e.stopPropagation();
+//             const oldScrollTop = this.scrollTop;
+//             this.scrollBy({ top: this.scrollTop * -1, behavior: "auto" });
+//             if (this.parentElement) {
+//                 this.parentElement.scrollBy({
+//                     top: e.deltaY + oldScrollTop,
+//                     behavior: "auto",
+//                 });
+//             }
+//         }
+//     };
+// }
