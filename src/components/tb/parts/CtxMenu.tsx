e1dee716db04cae10c37eacb5e51f2d83b8689f2
Marwan Hilal <marwan.hilal.dev+1@gmail.com>
2026-07-28 11:51:56 +0200

    fix: missing to add `GroupType.Radio` to sort group

--- src/components/tb/parts/CtxMenu.tsx
+++ src/components/tb/parts/CtxMenu.tsx
@@ -1,5 +1,6 @@
 import { CtxMenu as CtxMenuComponent } from "../../ctx-menu/CtxMenu.jsx";
 import { Api as ContextMenuApi } from "../../ctx-menu/domain/props.js";
+import { GroupType } from "../../ctx-menu/parts/Group.jsx";
 import { Col } from "../domain/cols/col.js";
 
 export function CtxMenu(props: { api: ContextMenuApi<Col> }) {
@@ -15,6 +16,7 @@ export function CtxMenu(props: { api: ContextMenuApi<Col> }) {
                 },
                 {
                     title: () => "ORDER",
+                    type: GroupType.Radio,
                     items: [
                         { label: () => "Ascending", onclick: () => {} },
                         { label: () => "Descending", onclick: () => {} },
