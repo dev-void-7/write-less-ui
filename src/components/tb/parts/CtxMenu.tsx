939e0442fcf291e2ffc6489f9eae76f3190509ed
Marwan Hilal <marwan.hilal.dev+1@gmail.com>
2026-07-28 23:16:04 +0200

    use expand and shrink icons

--- src/components/tb/parts/CtxMenu.tsx
+++ src/components/tb/parts/CtxMenu.tsx
@@ -2,6 +2,8 @@ import { CtxMenu as CtxMenuComponent } from "../../ctx-menu/CtxMenu.jsx";
 import { Api as ContextMenuApi } from "../../ctx-menu/domain/props.js";
 import { GroupType } from "../../ctx-menu/parts/Group.jsx";
 import { Col } from "../domain/cols/col.js";
+import { StachExpandVerticalIcon } from "../../icons/StachExpandVertical.jsx";
+import { StachShrinkVerticalIcon } from "../../icons/StachShrinkVertical.jsx";
 
 export function CtxMenu(props: { api: ContextMenuApi<Col> }) {
     console.log(props.api);
@@ -10,8 +12,16 @@ export function CtxMenu(props: { api: ContextMenuApi<Col> }) {
             groups={[
                 {
                     items: [
-                        { label: () => "extend column", onclick: () => {} },
-                        { label: () => "shrink column", onclick: () => {} },
+                        {
+                            label: () => "extend column",
+                            onclick: () => {},
+                            icon: <StachExpandVerticalIcon />,
+                        },
+                        {
+                            label: () => "shrink column",
+                            onclick: () => {},
+                            icon: <StachShrinkVerticalIcon />,
+                        },
                     ],
                 },
                 {
