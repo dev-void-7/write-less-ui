0ad5d45f195041eba30f634b718b8a572a54df56
Marwan Hilal <marwan.hilal.dev+1@gmail.com>
2026-07-29 23:04:21 +0200

    call `onSort` when sort is being set

--- src/components/tb/domain/state.ts
+++ src/components/tb/domain/state.ts
@@ -2,7 +2,7 @@ import { Cols } from "./cols/cols.js";
 import { Foots } from "./foots/foots.js";
 import { Api as CtxMenuApi } from "../../ctx-menu/domain/props.js";
 import { Props } from "./props.js";
-import { Accessor, createSignal, Setter } from "solid-js";
+import { Accessor, createSignal } from "solid-js";
 import { SortedBy } from "./sorted-by.js";
 import { Col } from "./cols/col.js";
 
@@ -12,7 +12,7 @@ export class State<S = any> {
     ctxMenu = {} as CtxMenuApi<Col<S>>;
     sorted?: {
         by: Accessor<SortedBy<S> | undefined>;
-        setBy: Setter<SortedBy<S> | undefined>;
+        setBy: (val: SortedBy<S> | undefined) => void;
     };
     tbWrapper!: HTMLDivElement;
     tb!: HTMLTableElement;
@@ -27,7 +27,13 @@ export class State<S = any> {
         this.foots = new Foots(props, this.cols.leafs, this.cols.orderedLeafs);
         if (props.onSort) {
             const [by, setBy] = createSignal<SortedBy<S> | undefined>(undefined);
-            this.sorted = { by, setBy };
+            this.sorted = {
+                by,
+                setBy: function (val: SortedBy<S> | undefined) {
+                    props.onSort?.(val);
+                    setBy(val);
+                },
+            };
         }
     }
 
