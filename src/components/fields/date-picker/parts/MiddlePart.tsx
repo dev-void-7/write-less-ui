89b15b1cca37edad045e303807337592db61568e
Marwan Hilal <marwan.hilal.dev+1@gmail.com>
2026-08-30 10:05:00 +0200

    add deselect icon button to `DatePicker` Component

--- src/components/fields/date-picker/parts/MiddlePart.tsx
+++ src/components/fields/date-picker/parts/MiddlePart.tsx
@@ -1,7 +1,8 @@
-import { createUniqueId } from "solid-js";
+import { createUniqueId, Show } from "solid-js";
 import { CalenderOutlineIcon } from "../../../icons/CalenderOutline.jsx";
 import { Calendar } from "./Calendar.jsx";
 import { useDatePickerContext } from "../contexts/state.js";
+import { XIcon } from "../../../icons/X.jsx";
 
 export function MiddlePart() {
     const calenderId = createUniqueId();
@@ -10,17 +11,24 @@ export function MiddlePart() {
 
     const inputWrapper = (
         <>
-            <button
-                id={merged.id}
-                class="wl--date-picker"
-                type="button"
-                ref={state.button}
-                popovertarget={calenderId}
-            >
-                {merged.startIcon}
-                <span class="wl--selected">{state.picked()}</span>
-                <CalenderOutlineIcon classList={{ "wl--icon-calender": true }} />
-            </button>
+            <div class="wl--date-picker-wrapper">
+                <button
+                    id={merged.id}
+                    class="wl--date-picker"
+                    type="button"
+                    ref={state.button}
+                    popovertarget={calenderId}
+                >
+                    {merged.startIcon}
+                    <span class="wl--selected">{state.picked()}</span>
+                    <CalenderOutlineIcon classList={{ "wl--icon-calender": true }} />
+                </button>
+                <Show when={state.picked() !== undefined}>
+                    <button class="wl--btn-deselect" onClick={() => state.setPicked(undefined)}>
+                        <XIcon classList={{ "wl--icon-deselect": true }} />
+                    </button>
+                </Show>
+            </div>
             <Calendar id={calenderId} />
         </>
     );
